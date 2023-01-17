c7d52c17fe25a4af9b2c2d0348baef37ccdb29af
Ilya Ivanov <aquarell.dev@gmail.com>
2023-01-17 08:14:22 -0800

    git test

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './styles/index.css';
-import './styles/tailwind.css';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './styles/index.css'
+import './styles/tailwind.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+	<React.StrictMode>
+		<p>123</p>
+		<App />
+	</React.StrictMode>
+)
