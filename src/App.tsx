a89e4111a871fa793f062625a6680f18186d9ca4
Ilya Ivanov <aquarell.dev@gmail.com>
2023-01-21 18:56:58 +0300

    added redux and theme

--- src/App.tsx
+++ src/App.tsx
@@ -5,30 +5,35 @@ import Layout from './components/layouts/Layout'
 import Analysis from './components/screens/Analysis'
 import Dashboard from './components/screens/Dashboard'
 import Settings from './components/screens/Settings'
+import { useTypedSelector } from './hooks/useTypedSelector'
 
 const App = () => {
+	const theme = useTypedSelector(state => state.preferences.theme)
+
 	return (
-		<BrowserRouter>
-			<Routes>
-				<Route
-					path='/'
-					element={<Layout />}
-				>
+		<main className={theme}>
+			<BrowserRouter>
+				<Routes>
 					<Route
-						index
-						element={<Dashboard />}
-					/>
-					<Route
-						path='/analysis'
-						element={<Analysis />}
-					/>
-					<Route
-						path='/settings'
-						element={<Settings />}
-					/>
-				</Route>
-			</Routes>
-		</BrowserRouter>
+						path='/'
+						element={<Layout />}
+					>
+						<Route
+							index
+							element={<Dashboard />}
+						/>
+						<Route
+							path='/analysis'
+							element={<Analysis />}
+						/>
+						<Route
+							path='/settings'
+							element={<Settings />}
+						/>
+					</Route>
+				</Routes>
+			</BrowserRouter>
+		</main>
 	)
 }
 
