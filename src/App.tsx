96035531c8069d08214901202f7906e7f0a04728
Ilya Ivanov <aquarell.dev@gmail.com>
2023-01-25 15:47:30 +0300

    route name changed

--- src/App.tsx
+++ src/App.tsx
@@ -23,7 +23,7 @@ const App = () => {
 							element={<Dashboard />}
 						/>
 						<Route
-							path='/analysis'
+							path='/analytics'
 							element={<Analysis />}
 						/>
 						<Route
