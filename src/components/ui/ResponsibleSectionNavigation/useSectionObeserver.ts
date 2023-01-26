1aabf2c4f7db269e922b1887b8bab6c3127035d8
Ilya Ivanov <aquarell.dev@gmail.com>
2023-01-26 14:11:30 +0300

    added on scroll animations

---
+++ src/components/ui/ResponsibleSectionNavigation/useSectionObeserver.ts
@@ -0,0 +1,44 @@
+import { useEffect } from 'react'
+import { Section } from './navigation.types'
+
+const useSectionObeserver = (
+	sections: Section[],
+	cb: (activeIndex: number) => void
+) => {
+	const ioCallback = (entries: IntersectionObserverEntry[]) => {
+		const intersectingEntries = entries
+			.map(entry => ({
+				entryId: entry.target.id,
+				intersects: entry.isIntersecting,
+			}))
+			.filter(e => e.intersects)
+
+		const lastIntersectingElementId = intersectingEntries
+			.map(e => e.entryId)
+			.at(-1)
+
+		const activeIndex = sections.findIndex(
+			section => section.scrollId === lastIntersectingElementId
+		)
+
+		cb(activeIndex)
+	}
+
+	const io = new IntersectionObserver(ioCallback, {
+		threshold: 1,
+	})
+
+	useEffect(() => {
+		const elements = sections.map(section =>
+			document.querySelector(`#${section.scrollId}`)
+		)
+
+		elements.forEach(element => element && io.observe(element))
+
+		return () => io.disconnect()
+	}, [])
+
+	return {}
+}
+
+export default useSectionObeserver
